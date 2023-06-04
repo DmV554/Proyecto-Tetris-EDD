ed33b001dd658f8195f2b98b645d6948d3530655
DmV554 <daniel3318bl@gmail.com>
2023-06-03 23:54:07 -0400

    Avance en la estructura del sistema de seleccion del menu

--- main.c
+++ main.c
@@ -1,14 +1,39 @@
 #include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include "list.h"
 #define ENTER 10
 
-void ejecutarSeleccion(int);
+void menu(int*);
+void ejecutarSeleccion(int, List*);
+void funcionJugar();
+void mostrarPuntajes();
+void mostrarBloques();
+void inicializarBloques(List*);
 
-int main() {
+int main() { 
+    List*listaBloques = createList();
+    inicializarBloques(listaBloques);
+
+    int seleccion = 1;
+    menu(&seleccion);
+
+    ejecutarSeleccion(seleccion, listaBloques);
+
+
+    return 0;
+}
+
+
+void menu(int*seleccionPtr) {
     initscr(); // Inicializar ncurses
     keypad(stdscr, TRUE); // Habilitar el uso de teclas especiales
 
     int opcion = 0;
-    int seleccion = 1;
+    int seleccion = *seleccionPtr;
 
     while (opcion != ENTER) {
         clear(); // Limpiar la pantalla
@@ -35,9 +60,7 @@ int main() {
                 break;
             case ENTER:
                 opcion = tecla;
-                
-                ejecutarSeleccion(seleccion);
-
+                *seleccionPtr = seleccion;
                 break;
             default:
                 printw("Opcion invalida. Intente de nuevo.\n");
@@ -46,14 +69,30 @@ int main() {
     }
 
     endwin(); // Finalizar ncurses
-
-    printw("Seleccionaste la opcion %d\n", seleccion);
     refresh(); // Actualizar la pantalla
-
-    return 0;
 }
 
 
-void ejecutarSeleccion(int) {
-    return;
+void ejecutarSeleccion(int seleccion, List*listaBloques) {
+    
+    switch (seleccion) {
+        case 1:
+            funcionJugar();
+        break;
+
+        case 2:
+            mostrarPuntajes();
+        break;
+
+        case 3:
+            mostrarBloques();
+        break;
+
+        case 4:
+            return;
+        break;
+        
+        default:
+        break;
+    }
 }
