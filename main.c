041673ea36b3b4a9a881a80c118355928cee17f9
DmV554 <daniel3318bl@gmail.com>
2023-06-30 00:43:04 -0400

    Funciones para reproducir musica del tetris clasico, mas el archivo mp3

--- main.c
+++ main.c
@@ -8,7 +8,11 @@
 #include "bloques.h"
 #include "treemap.h"
 #include <unistd.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+#include <SDL2/SDL_timer.h>
 #include <stdbool.h>
+#undef main
 
 #define ENTER 10
 #define ALTO_JUEGO 20
@@ -42,7 +46,7 @@ void asignarJugador(size_t, TreeMap*, char*);
 void selectorDificultad(int*, char*);
 void imprimirPuntajes(char*, TreeMap*);
 void mostrarInstrucciones();
-
+void cargarMusica();
 
 int lower_than_int(void * key1, void * key2) {
     if(*(int*)key1 > *(int*)key2) return 1;
@@ -50,7 +54,9 @@ int lower_than_int(void * key1, void * key2) {
 }
 
 
-int main() { 
+int main(int argc, char *argv[]) { 
+    cargarMusica();
+
     TreeMap* Jugadores = createTreeMap(lower_than_int);
     List*listaBloques = createList();
     inicializarBloques(listaBloques);
@@ -63,26 +69,40 @@ int main() {
     
     free(listaBloques);
     free(Jugadores);
+
+    SDL_Quit();
+
     return 0;
 }
 
 void inicializarBloques(List*listaBloques) {
-
     inicializarCuadrado(listaBloques);
-
     inicializarL1(listaBloques);
-
     inicializarL2(listaBloques);
-
     inicializarI(listaBloques);
-
     inicializarT(listaBloques);
-
     inicializarZ(listaBloques);
-
     inicializarS(listaBloques);
+}
 
-    //dou();
+void cargarMusica() {
+    int result = 0;
+    int flags = MIX_INIT_MP3;
+
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        printf("Failed to init SDL\n");
+        exit(1);
+    }
+
+    if (flags != (result = Mix_Init(flags))) {
+        printf("Could not initialize mixer (result: %d).\n", result);
+        printf("Mix_Init: %s\n", Mix_GetError());
+        exit(1);
+    }
+
+    Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 640);
+    Mix_Music *music = Mix_LoadMUS("musica_tetris.mp3");
+    Mix_PlayMusic(music, 1);
 }
 
 void imprimirPuntajes(char * nivel, TreeMap * Jugadores){
